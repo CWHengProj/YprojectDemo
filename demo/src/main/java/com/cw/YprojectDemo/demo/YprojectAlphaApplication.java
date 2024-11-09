f84b3850caba346601bfa302de7243b2f29fd10a
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-09 15:30:33 +0800

    Commit in case

--- demo/src/main/java/com/cw/YprojectDemo/demo/YprojectAlphaApplication.java
+++ demo/src/main/java/com/cw/YprojectDemo/demo/YprojectAlphaApplication.java
@@ -1,5 +1,9 @@
 package com.cw.YprojectDemo.demo;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +14,23 @@ public class YprojectAlphaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(YprojectAlphaApplication.class);//calls the class
-		System.out.println("Starting application on port 8080..");
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")){
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		System.out.printf("Starting application on %s\n",port);
 		app.run(args);
 	}
-	/* @Bean
+	@Bean
 	public CommonsRequestLoggingFilter log(){
 		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
 		logger.setIncludeClientInfo(true);
 		logger.setIncludeQueryString(true);
 		return logger;
-	} */
+	}
+	//wait for loggers lesson
 
 }
-//application.properties;//to add or not to add?
-//logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter= DEBUG;//to add or not to add?
+
