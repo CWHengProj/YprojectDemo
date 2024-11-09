fc3e29cf5c035cd9cc3521f5fa3379d438290e9d
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-09 13:05:52 +0800

    Added controllers
    To better understand controllers before implementing

--- demo/src/main/java/com/cw/YprojectDemo/demo/YprojectAlphaApplication.java
+++ demo/src/main/java/com/cw/YprojectDemo/demo/YprojectAlphaApplication.java
@@ -2,6 +2,8 @@ package com.cw.YprojectDemo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication //indicates that it is a springboot application - autoconfig and scans for other components
 public class YprojectAlphaApplication {
@@ -11,5 +13,14 @@ public class YprojectAlphaApplication {
 		System.out.println("Starting application on port 8080..");
 		app.run(args);
 	}
+	/* @Bean
+	public CommonsRequestLoggingFilter log(){
+		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);
+		return logger;
+	} */
 
 }
+//application.properties;//to add or not to add?
+//logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter= DEBUG;//to add or not to add?
